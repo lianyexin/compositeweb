20dcb2062db5ae490c00150b7b40fb8a395ea08a
shiwei <shiwei@csrd.cn>
2018-07-04 18:11:24 +0800

    data module

---
+++ dataservice/src/main/java/com/wstone/DataApplication.java
@@ -0,0 +1,10 @@
+package com.wstone;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shiwei on 2018/7/4.
+ */
+@SpringBootApplication
+public class DataApplication {
+}
