50ae93e9ba1911742c1a2de96a01b70d82737c22
shiwei <shiwei@csrd.cn>
2018-07-16 16:59:34 +0800

    TEST START

--- dataservice/src/main/java/com/wstone/DataApplication.java
+++ dataservice/src/main/java/com/wstone/DataApplication.java
@@ -1,5 +1,6 @@
 package com.wstone;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -7,4 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class DataApplication {
+
+    public static void main(String []args){
+        SpringApplication.run(DataApplication.class);
+    }
+
 }
