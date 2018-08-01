819e5d32b0408bb3313cd40985f3e5ac9f56e295
shiwei <shiwei@csrd.cn>
2018-08-01 17:55:50 +0800

    add configuration server

---
+++ configserver/src/main/java/com/wstone/ConfigApp.java
@@ -0,0 +1,21 @@
+package com.wstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by shiwei on 2018/8/1.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+
+public class ConfigApp {
+
+    public static void main(String []args){
+        SpringApplication.run(ConfigApp.class);
+    }
+
+}
